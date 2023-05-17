6ba6340fd5524855a05fd5aacfee9fb117f5e1d0
BuiKiet <roiyano1utl@gmail.com>
2023-05-17 14:26:22 +0700

    1. connect to db

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-const express = require("express");
-import route from "./routers";
+const express = require("express"); // ES5
+import route from "./routers"; // ES6
+import connectDb from "./config/db";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+// Connect to db
+connectDb();
 
 route(app);
 
